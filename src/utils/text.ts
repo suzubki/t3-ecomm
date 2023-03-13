ed15711e525d27abf0882b70c18f5b9128472b81
Darwin_N19 <darwinccn25@gmail.com>
2023-03-13 18:49:55 -0500

    slugifyText allows you improve de string for SEO usage

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -1,3 +1,12 @@
+import slugify from "slugify";
+
 export const capitalizeFirstLetter = (str: string) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export const slugifyText = (str: string) => {
+  return slugify(str, {
+    lower: true,
+    remove: /[*+~.()'"!:@]/g
+  });
 }
