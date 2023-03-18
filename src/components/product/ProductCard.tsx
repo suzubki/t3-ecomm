3b0e1f98686850b987ae2761b1d377f89f4837b4
Darwin_N19 <darwinccn25@gmail.com>
2023-03-18 17:39:30 -0500

    feat: changing the width for more responsivness

--- src/components/product/ProductCard.tsx
+++ src/components/product/ProductCard.tsx
@@ -10,9 +10,9 @@ interface Props {
 
 export const ProductCard: React.FC<Props> = ({ product }) => {
   return (
-    <Link href={`/productos/${product.id}`} className="relative block w-full min-w-[16rem] max-w-[18rem]">
+    <Link href={`/productos/${product.id}`} className="relative block flex-1 min-w-[14rem] max-w-[16rem] md:min-w-[16rem] md:max-w-[18rem]">
       <div className="flex h-full flex-col rounded-md pb-2 shadow-sm transition-all duration-300 ease-in hover:shadow-md">
-        {/* Contenedor de la imagen */}
+        {/* Image container */}
         <div className="relative flex h-64 items-center">
           <div className="absolute top-8 right-4 z-50 flex flex-col items-center gap-1">
             <AiOutlineHeart className="h-7 w-auto rounded-full p-1 text-gray-600 transition-all duration-200 ease-in hover:bg-red-500 hover:text-light-primary" />
@@ -22,19 +22,18 @@ export const ProductCard: React.FC<Props> = ({ product }) => {
             src={`${product.image}`}
             className="object-contain p-12"
             fill
-            sizes="(min-width: 60em) 10vw,
-                    (min-width: 28em) 10vw,
-                    10vw"
+            sizes="(min-width: 60em) 12vw,
+                    (min-width: 28em) 12vw,
+                    50vw"
           />
         </div>
-        {/* Información del producto */}
+        {/* Product information */}
         <div className="mt-1 flex flex-1 flex-col justify-between gap-3 py-2 px-4">
-          {/* Nombre del producto */}
+          {/* Products name */}
           <h3 className="text-center text-base font-semibold leading-5 tracking-[-0.5px]">
             {product.title}
           </h3>
-          {/* Precio y Agregar */}
-          {/* Precio */}
+          {/* Price */}
           <div className="flex items-end justify-center gap-2">
             <span className="text-xs text-gray-500 line-through">
               $ {(product.price + 10).toFixed(2)}
