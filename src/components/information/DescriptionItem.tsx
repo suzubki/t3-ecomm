9ed4d8dbd28434c215b0033bc6f9f67a8381757b
Darwin_N19 <darwinccn25@gmail.com>
2023-03-18 17:37:22 -0500

    fix: the icon shrinks in mobile

--- src/components/information/DescriptionItem.tsx
+++ src/components/information/DescriptionItem.tsx
@@ -8,9 +8,13 @@ interface Props{
 export const DescriptionItem: React.FC<Props> = ({ title, description }) => {
   return (
     <div className="flex flex-col gap-4">
-      <h3 className="flex items-center gap-1 mx-12 py-1 px-4 border-b-solid border-b-2 border-b-gray-300 text-2xl font-medium tracking-[-1px]">
-        <BsFillXDiamondFill className="w-4 h-4" />{title}</h3>
-      <p className="mx-20 tracking-tighter">{description}</p>
+      <h3 className="flex items-center leading-6 gap-2 mx-6 sm:mx-12 py-1 px-4 border-b-solid border-b-2 border-b-gray-300 text-xl sm:text-2xl font-medium tracking-[-1px]">
+        <div>
+          <BsFillXDiamondFill className="w-3 h-3 sm:w-4 sm:h-4" />
+        </div>
+        {title}
+      </h3>
+      <p className="mx-10 sm:mx-20 tracking-tighter">{description}</p>
     </div>
   )
 }
