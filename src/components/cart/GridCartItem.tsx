58bb09cce5a5cefad321800f9d213b8b985b0659
Darwin_N19 <darwinccn25@gmail.com>
2023-03-28 23:42:34 -0500

    feat: Adding a width  responsive for small devices

--- src/components/cart/GridCartItem.tsx
+++ src/components/cart/GridCartItem.tsx
@@ -7,7 +7,7 @@ interface Props{
 
 export const GridCartItem: React.FC<Props> = ({ items }) => {
   return (
-    <div className="h-[calc(100vh-4.5rem-12rem)] overflow-auto flex flex-col divide-y-2">
+    <div className={`flex-1 sm:max-h-none sm:h-[calc(100vh-4.5rem-12rem)] overflow-auto flex flex-col divide-y-2`}>
       {
         items.map((item)=> (
           <CartItem item={item} key={`${item.id} - ${item.name} - ${item.size} - ${item.quantity}`} />
