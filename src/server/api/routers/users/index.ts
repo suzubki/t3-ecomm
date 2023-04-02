ddc51c1dfea0a7ed3e4fe52b216cf987500de3b3
Darwin_N19 <darwinccn25@gmail.com>
2023-04-02 12:56:48 -0500

    wip: login user route

--- src/server/api/routers/users/index.ts
+++ src/server/api/routers/users/index.ts
@@ -1,41 +1,24 @@
+import { hash } from "bcrypt";
 import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "../../trpc";
-
-import { compare } from "bcrypt";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const userRouter = createTRPCRouter({
   create: publicProcedure.input(z.object({
-    name: z.string(),
+    name: z.string().min(4),
     email: z.string().email(),
     password: z.string().min(8),
   })).mutation(async ({ ctx, input }) => {
     const { email, password, name } = input;
+    // encrypt
+    const hashedPassword = await hash(password, Number(process.env.SALT_ROUNDS) || 10);
+
     const user = await ctx.prisma.user.create({
       data: {
         email,
-        password,
+        password: hashedPassword,
         name,
       },
     });
-
-    return user;
-  }),
-  // Login with email and password
-  login: publicProcedure.input(z.object({
-    email: z.string().email(),
-    password: z.string().min(8),
-  })).mutation(async ({ ctx, input }) => {
-    const { email, password } = input;
-    const user = await ctx.prisma.user.findUnique({
-      where: {
-        email,
-      },
-    });
-    if (!user) return null
-
-    // Compare password with hash
-    const isPasswordOk = await compare(password, user.password!)
-    if (!isPasswordOk) return null
 
     return user;
   }),
